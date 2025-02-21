c6442e86c9809b0e1a7c9e8e43e36a96ed54d015
harith <37389526+harithzamri@users.noreply.github.com>
2025-02-21 17:09:59 +0800

    feat: hide env

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
 
-const URL =
-  "https://bitbucket.org/!api/2.0/snippets/tawkto/aA8zqE/4f62624a75da6d1b8dd7f70e53af8d36a1603910/files/webstats.json";
+const URL = process.env.URL;
 
 class HttpFetcher {
   async fetchData(url) {
